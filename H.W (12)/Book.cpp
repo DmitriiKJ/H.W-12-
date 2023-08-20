fe259137d9f5d7e489826d312c5be2b9268097ca
kurbatovdima <kurbatovdima2006@gmail.com>
2023-08-20 13:46:44 +0200

    add Book.cpp

--- H.W (12)/Book.cpp
+++ H.W (12)/Book.cpp
@@ -1,1 +1,27 @@
 #include "Book.h"
+
+Book::Book()
+{
+    res = "Empty";
+}
+
+Book::Book(string n, string a, int y, string i, string r): Publication(n, a, y, i)
+{
+    res = r;
+}
+
+void Book::set_res(string r)
+{
+    res = r;
+}
+
+string Book::get_res() const
+{
+    return res;
+}
+
+void Book::show() const
+{
+    Publication::show();
+    cout << "Res: " << res << endl;
+}
