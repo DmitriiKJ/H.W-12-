fac671fdbf0ec85ff8176d2e6187b0990c5be94e
kurbatovdima <kurbatovdima2006@gmail.com>
2023-08-20 13:56:28 +0200

    add Magazine.h

---
+++ H.W (12)/Magazine.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Publication.h"
+#include <vector>
+
+class Magazine : public Publication
+{
+protected:
+	vector<string> articles;
+
+public:
+	Magazine();
+	Magazine(string n, string a, int y, string i);
+
+	void add_ar(string a);
+	vector<string> get_articles()const;
+
+	void show()const;
+};
+
