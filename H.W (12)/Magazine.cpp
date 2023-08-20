001dbb11e979b1bfbcb779d2e42a6202c698a427
kurbatovdima <kurbatovdima2006@gmail.com>
2023-08-20 13:59:13 +0200

    add Magazine.cpp

--- H.W (12)/Magazine.cpp
+++ H.W (12)/Magazine.cpp
@@ -1,1 +1,29 @@
 #include "Magazine.h"
+
+Magazine::Magazine()
+{
+}
+
+Magazine::Magazine(string n, string a, int y, string i): Publication(n, a, y, i)
+{
+}
+
+void Magazine::add_ar(string a)
+{
+    articles.push_back(a);
+}
+
+vector<string> Magazine::get_articles() const
+{
+    return articles;
+}
+
+void Magazine::show() const
+{
+    Publication::show();
+    cout << "Articles: " << endl;
+    for (int i = 0; i < articles.size(); i++)
+    {
+        cout << articles[i] << endl;
+    }
+}
